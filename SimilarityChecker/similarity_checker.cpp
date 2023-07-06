30d572243822053aeed81bcf5597ad8ed1e886c9
j1221.park <j1221.park@samsung.com>
2023-07-06 13:58:43 +0900

    [feature] add api / TDD for alphaPoint

--- SimilarityChecker/similarity_checker.cpp
+++ SimilarityChecker/similarity_checker.cpp
@@ -1,5 +1,7 @@
 #include <string>
+#include <set>
 #include <algorithm>
+
 using namespace std;
 
 class SimilarityChecker
@@ -24,6 +26,49 @@ public:
 		}
 
 		return getPartialPoint(max(input1.length(), input2.length()), min(input1.length(), input2.length()));
+	}
+
+	int getAlphaPoint()
+	{
+		set<char> candAlpha;
+
+		for (char alpha : input1)
+		{
+			if (alpha < 'A' || alpha > 'Z') return 0;
+			candAlpha.insert(alpha);
+		}
+		for (char alpha : input2)
+		{
+			if (alpha < 'A' || alpha > 'Z') return 0;
+			candAlpha.insert(alpha);
+		}
+
+		int totalCnt = candAlpha.size();
+		int sameCnt = 0;
+
+		for (char alpha : candAlpha)
+		{
+			bool found1 = false;
+			for (char ch : input1)
+			{
+				if (alpha == ch)
+				{
+					found1 = true;
+					break;
+				}
+			}
+			bool found2 = false;
+			for (char ch : input2)
+			{
+				if (alpha == ch)
+				{
+					found2 = true;
+					break;
+				}
+			}
+			if (found1 && found2) ++sameCnt;
+		}
+		return sameCnt * 40 / totalCnt;
 	}
 
 	string input1;
